9d870af1d23d4710900ad6bbc3b6e468374b7167
Khalid <zkac429@live.rhul.ac.uk>
2024-07-05 20:42:09 +0100

    Created a rough way to simulate sand dropping which still needs a lot of refining

--- particles/particles.cpp
+++ particles/particles.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <raylib.h>
 
+#define CELL_ROWS 80
+#define CELL_COLUMNS 80
+
 typedef struct Cell {
 	int width;
 	int height;
@@ -8,22 +11,30 @@ typedef struct Cell {
 	Color colour;
 } Cell;
 
-const int screenWidth = 1280;
-const int screenHeight = 720;
+typedef struct Sand {
+	int width;
+	int height;
+	Color colour = YELLOW;
+} Sand;
+
+const int screenWidth = 800;
+const int screenHeight = 800;
 
 int main(void)
 {
 	// Initialisation
 
-	Cell cells[5][5];
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
-			cells[i][j].width = screenWidth / 5;
-			cells[i][j].height = screenHeight / 5;
+	Cell cells[CELL_ROWS][CELL_COLUMNS];
+	for (int i = 0; i < CELL_ROWS; i++) {
+		for (int j = 0; j < CELL_COLUMNS; j++) {
+			cells[i][j].width = screenWidth / CELL_ROWS;
+			cells[i][j].height = screenHeight / CELL_COLUMNS;
 			cells[i][j].active = false;
 			cells[i][j].colour = RAYWHITE;
 		}
 	}
+
+	Sand sand[CELL_ROWS][CELL_COLUMNS];
 
 	InitWindow(screenWidth, screenHeight, "display");
 	SetTargetFPS(60);
@@ -31,6 +42,39 @@ int main(void)
 	// Main Game loop
 	while (!WindowShouldClose()) {
 		// Update
+		for (int i = CELL_ROWS - 1; i >= 0; i--) {
+			for (int j = 0; j < CELL_COLUMNS; j++) {
+				if (cells[i][j].active && !cells[i + 1][j].active) {
+					int newI = i + 1;
+					if (newI < CELL_ROWS) {
+						cells[newI][j].colour = sand[i][j].colour;
+						cells[newI][j].active = true;
+						cells[i][j].colour = RAYWHITE;
+						cells[i][j].active = false;
+					}
+				}
+				else if (cells[i][j].active && !cells[i + 1][j - 1].active) {
+					int newI = i + 1;
+					int newJ = j - 1;
+					if (newI < CELL_ROWS && newJ < CELL_COLUMNS) {
+						cells[newI][newJ].colour = sand[i][j].colour;
+						cells[newI][newJ].active = true;
+						cells[i][j].colour = RAYWHITE;
+						cells[i][j].active = false;
+					}
+				}
+				else if (cells[i][j].active && !cells[i + 1][j + 1].active) {
+					int newI = i + 1;
+					int newJ = j + 1;
+					if (newI < CELL_ROWS && newJ < CELL_COLUMNS) {
+						cells[newI][newJ].colour = sand[i][j].colour;
+						cells[newI][newJ].active = true;
+						cells[i][j].colour = RAYWHITE;
+						cells[i][j].active = false;
+					}
+				}
+			}
+		}
 
 		// Draw
 		BeginDrawing();
@@ -39,29 +83,23 @@ int main(void)
 
 		DrawFPS(30, 30);
 
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
+		for (int i = 0; i < CELL_ROWS; i++) {
+			for (int j = 0; j < CELL_COLUMNS; j++) {
 				int x = j * cells[i][j].width;
 				int y = i * cells[i][j].height;
-				if (CheckCollisionPointRec(GetMousePosition(), { (float)x, (float)y, (float)cells[i][j].width, (float)cells[i][j].height }) && IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-					cells[i][j].colour = RED;
+				if (CheckCollisionPointRec(GetMousePosition(), { (float)x, (float)y, (float)cells[i][j].width, (float)cells[i][j].height }) && IsMouseButtonDown(MOUSE_BUTTON_LEFT) && !cells[i][j].active) {
+					cells[i][j].colour = sand[i][j].colour;
 					cells[i][j].active = true;
 				}
-				else
-				{
-					cells[i][j].colour = RAYWHITE;
-					cells[i][j].active = false;
-				}
+
 				DrawRectangle(x, y, cells[i][j].width, cells[i][j].height, cells[i][j].colour);
-				DrawRectangleLines(x, y, cells[i][j].width, cells[i][j].height, BLACK);
+				DrawRectangleLines(x, y, cells[i][j].width, cells[i][j].height, RAYWHITE);
 			}
 		}
 
 		EndDrawing();
 	}
 
-
 	// De-Initialisation
 	CloseWindow();
-
 }
